796c40593166627364f751334137089fd489a1b3
Leen Qtieshat <leen20qtt@gmail.com>
2023-04-30 21:41:36 +0300

    changed the url  commit

--- src/http/http.js
+++ src/http/http.js
@@ -1,18 +1,18 @@
-import axios from "axios";
-import { authInterceptor, requestErrorHandler } from "./requestInterceptors";
-import { responseErrorHandler, responseParser } from "./responseInterceptors";
-import { isProd } from "./env";
+import axios from 'axios';
+import { authInterceptor, requestErrorHandler } from './requestInterceptors';
+import { responseErrorHandler, responseParser } from './responseInterceptors';
+import { isProd } from './env';
 
 const BASE_URL = isProd
-  ? "https://ticketak-api.herokuapp.com/api/v1"
-  : "http://localhost:5000/api/v1";
+  ? 'https://backend-ticketak.onrender.com/api/v1'
+  : 'http://localhost:5000/api/v1';
 // TODO change this to the heroku url
 
 console.log(isProd, BASE_URL);
 
 const http = axios.create({
   baseURL: BASE_URL,
-  timeout: 2000,
+  timeout: 2000
 });
 
 http.interceptors.request.use(authInterceptor, requestErrorHandler);
